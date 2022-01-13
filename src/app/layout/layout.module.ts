87a99057454a56e6efa1f71ca8e7b5e4d24ffccb
johnj974 <jjdug@hotmail.com>
2022-01-13 19:19:33 +0000

    fontsAdded/FooterLayout/Links+ComponentsConnected

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FooterComponent, NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [FooterComponent, NavbarComponent],
 })
 export class LayoutModule {}
